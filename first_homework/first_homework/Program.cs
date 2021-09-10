917b0d955301832ac25917e3137b59156791a464
SanoDolorato <90412284+SanoDolorato@users.noreply.github.com>
2021-09-10 22:53:31 +0300

    Написали программный код

--- first_homework/first_homework/Program.cs
+++ first_homework/first_homework/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//Дано целое число, большее 999.
+//Используя одну операцию деления нацело
+//и одну операцию взятия остатка от деления,
+//найти цифру, соответствующую разряду тысяч в записи этого числа.
+
+
+using System;
 
 namespace first_homework
 {
@@ -6,7 +12,19 @@ namespace first_homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите число(>999)\n");
+            int first_number = int.Parse(Console.ReadLine());
+            if(first_number<1000)
+                Console.WriteLine("Ошибка.Число не подходит по условию");
+            else
+            {
+                int k = first_number / 1000;
+                k = k % 10;
+                Console.WriteLine($"Цифра, соответствующая разряду тысяч в записи этого числа:  {k}");
+            }
+
+            
+
         }
     }
 }
